7ad01d9d37cd239acf0f2a8df0d2af2a003b0e11
miguelAg0st1nh0 <miguel1995jordao@icloud.com>
2024-10-04 13:34:28 +0100

    Added followers list View Controller Added a general view controller for alerts created custom buttons, body and labels to be used within Alert VC added a UIView Controller extension

---
+++ GitHubFollowers/GFTitleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GFTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by Miguel Agostinho on 04/10/2024.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
